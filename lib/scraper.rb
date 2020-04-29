87175e49a8c090cd3de1cd070792571a4678cbf7
aurel fontaine <ajfame11@gmail.com>
2020-04-29 17:42:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,6 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-puts doc.css(".headline-26OIBN")
+doc = Nokogiri::HTML(html)
+ 
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
